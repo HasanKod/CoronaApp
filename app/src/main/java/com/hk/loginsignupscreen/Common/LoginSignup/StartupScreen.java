226bc8330c8efe94922c90ada6a9877d7ef36b9a
HaSaN <s1122269@student.hsleiden.nl>
2021-10-01 17:42:44 +0200

    Login screen added - animation from StartupScreen to Login screen added - colors updated - strings updated - images added

--- app/src/main/java/com/hk/loginsignupscreen/Common/LoginSignup/StartupScreen.java
+++ app/src/main/java/com/hk/loginsignupscreen/Common/LoginSignup/StartupScreen.java
@@ -2,7 +2,11 @@ package com.hk.loginsignupscreen.Common.LoginSignup;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActivityOptions;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Pair;
+import android.view.View;
 import android.view.WindowManager;
 
 import com.hk.loginsignupscreen.R;
@@ -14,5 +18,17 @@ public class StartupScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_startup_screen);
+    }
+
+    public void callLoginScreen(View view) {
+
+        Intent intent = new Intent(getApplicationContext(), Login.class);
+//      Pair[1]: number of elements you want to animate
+        Pair[] pairs = new Pair[1];
+        pairs[0] = new Pair<View, String>(findViewById(R.id.login_btn), "transition_login");
+
+        ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(StartupScreen.this, pairs);
+
+        startActivity(intent);
     }
 }
