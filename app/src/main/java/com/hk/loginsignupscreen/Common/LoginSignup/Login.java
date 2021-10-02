1e10258280d5b2c21584b639780a5813b5d41380
HaSaN <s1122269@student.hsleiden.nl>
2021-10-02 14:42:57 +0200

    Fullscreen option added to login screen

--- app/src/main/java/com/hk/loginsignupscreen/Common/LoginSignup/Login.java
+++ app/src/main/java/com/hk/loginsignupscreen/Common/LoginSignup/Login.java
@@ -3,6 +3,7 @@ package com.hk.loginsignupscreen.Common.LoginSignup;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 
 import com.hk.loginsignupscreen.R;
 
@@ -11,6 +12,7 @@ public class Login extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_login);
     }
 }
