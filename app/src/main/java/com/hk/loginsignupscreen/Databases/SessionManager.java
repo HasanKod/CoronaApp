ff1540aaaedb2f510ae23c2597647dcae2af31dc
HaSaN <s1122269@student.hsleiden.nl>
2021-10-10 22:11:50 +0200

    Shared preferences added

---
+++ app/src/main/java/com/hk/loginsignupscreen/Databases/SessionManager.java
@@ -0,0 +1,10 @@
+package com.hk.loginsignupscreen.Databases;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import java.util.HashMap;
+
+public class SessionManager {
+    
+}
